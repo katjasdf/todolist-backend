1938a460323791b3981b1604d3b051930825b0d7
katjasdf <katja.hinkkanen@outlook.com>
2020-05-17 20:04:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const Todo = require('./models/todo')
+
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+
+app.get('/api/todos', (request, response) => {
+  Todo.find({}).then(todos => {
+    response.json(todos.map(todo => todo.toJSON()))
+    console.log(todos)
+  })
+})
+
+app.get('/api/todos/:id', (request, response, next) => {
+    Todo.findById(request.params.id)
+    .then(todo => {
+        if (todo) {
+            response.json(todo)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/todos', (request, response) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const todo = new Todo({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  todo.save().then(savedNote => {
+    response.json(savedNote.toJSON())
+  })
+})
+
+app.delete('/api/todos/:id', (request, response, next) => {
+    Todo.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/todos/:id', (request, response, next) => {
+    const body = request.body
+
+    const todo = {
+        content: body.content,
+        important: body.important,
+    }
+
+    Todo.findByIdAndUpdate(request.params.id, todo, { new: true })
+        .then(updatedTodo => {
+            response.json(updatedTodo)
+        })
+        .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-lm2d7.mongodb.net/todo-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const todoSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+
+const todo = new Todo({
+  content: 'Callback-functions suck',
+  date: new Date(),
+  important: true,
+})
+
+/*
+todo.save().then(response => {
+  console.log('todo saved!');
+  mongoose.connection.close();
+})
+*/
+
+Todo.find({}).then(result => {
+  result.forEach(todo => {
+    console.log(todo)
+  })
+  mongoose.connection.close()
+})
